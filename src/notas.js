59a9dbcb274c757fe98f17c8e0d844d96782ca4a
martinteppa <martinteppa@gmail.com>
2021-03-04 17:34:56 -0300

    agregando_calificaciones

---
+++ src/notas.js
@@ -0,0 +1,157 @@
+const notas = [
+  {
+    nombre: "algoritmos y estructura de datos",
+    nota: 8,
+    año: "primero",
+  },
+  {
+    nombre: "analisis matematico I",
+    nota: 7,
+    año: "primero",
+  },
+  {
+    nombre: "arquitectura de computadoras",
+    nota: 7,
+    año: "primero",
+  },
+  {
+    nombre: "Fisica I",
+    nota: 7,
+    año: "primero",
+  },
+  {
+    nombre: "Matematica Discreta",
+    nota: 7,
+    año: "primero",
+  },
+  {
+    nombre: "Sistemas y Organizaciones",
+    nota: 6,
+    año: "primero",
+  },
+  {
+    nombre: "Algebra y Geometria analitica",
+    nota: 8,
+    año: "primero",
+  },
+  {
+    nombre: "Analisis de sistemas",
+    nota: 8,
+    año: "segundo",
+  },
+  {
+    nombre: "Analisis matematico II",
+    nota: 7,
+    año: "segundo",
+  },
+  {
+    nombre: "Fisica II",
+    nota: 8,
+    año: "segundo",
+  },
+  {
+    nombre: "Ingles I",
+    nota: 7,
+    año: "segundo",
+  },
+  {
+    nombre: "Paradigmas de Programacion",
+    nota: 7,
+    año: "segundo",
+  },
+  {
+    nombre: "Quimica",
+    nota: 9,
+    año: "segundo",
+  },
+  {
+    nombre: "Sintanxis y Semantica de los Lenguajes",
+    nota: 9,
+    año: "segundo",
+  },
+  {
+    nombre: "Sistemas de representacion",
+    nota: 6,
+    año: "segundo",
+  },
+  {
+    nombre: "Sistemas operativos",
+    nota: 8,
+    año: "segundo",
+  },
+  {
+    nombre: "Comunicaciones",
+    nota: 7,
+    año: "tercero",
+  },
+  {
+    nombre: "Diseño de Sistemas",
+    nota: null,
+    año: "tercero",
+  },
+  {
+    nombre: "Economia",
+    nota: 8,
+    año: "tercero",
+  },
+  {
+    nombre: "Gestion de Datos",
+    nota: 10,
+    año: "tercero",
+  },
+  {
+    nombre: "Ing. y Sociedad",
+    nota: 10,
+    año: "tercero",
+  },
+  {
+    nombre: "Ingles II",
+    nota: 8,
+    año: "tercero",
+  },
+  {
+    nombre: "Matematica Superior",
+    nota: 9,
+    año: "tercero",
+  },
+  {
+    nombre: "Probabilidades y Estadisticas",
+    nota: 7,
+    año: "tercero",
+  },
+  {
+    nombre: "Administracion de Recursos",
+    nota: 7,
+    año: "cuarto",
+  },
+  {
+    nombre: "Bases de datos Avanzada",
+    nota: 6,
+    año: "cuarto",
+  },
+  {
+    nombre: "Ingenieria de Software",
+    nota: 9,
+    año: "cuarto",
+  },
+  {
+    nombre: "Investigacion Operativa",
+    nota: 7,
+    año: "cuarto",
+  },
+  {
+    nombre: "Legislacion",
+    nota: null,
+    año: "cuarto",
+  },
+  {
+    nombre: "Redes de informacion",
+    nota: 7,
+    año: "cuarto",
+  },
+  {
+    nombre: "Simulacion",
+    nota: null,
+    año: "cuarto",
+  },
+];
